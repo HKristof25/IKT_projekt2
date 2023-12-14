4a987e1233152bc76f185961998676e0a7965ae7
DavidC112 <144622133+DavidC112@users.noreply.github.com>
2023-12-14 11:51:07 +0100

    .txt-be írás

--- Beolvas.cs
+++ Beolvas.cs
@@ -67,65 +67,85 @@ namespace Alkatreszek
                             cpu[3] = StringInput("Add meg az órajelét: ");
                             cpu[4] = IntInput("Add meg a  magok számát: ");
                             cpu[5] = IntInput("Add meg az árát: ");
+                            using (StreamWriter OutputFile = new StreamWriter("alkatreszek.txt", true))
+                            {
+                                OutputFile.Write($"{cpu[0]},{cpu[1]},{cpu[2]},{cpu[3]},{cpu[4]},{cpu[5]}\n");
+                            }
                             break;
                         case "motherboard":                                                             //motherboard
                             motherb[1] = StringInput("Add meg a gyártóját: ");
                             motherb[2] = StringInput("Add meg a típusát: ");
                             motherb[3] = StringInput("Add meg a foglalatot: ");
                             motherb[4] = IntInput("Add meg az árát: ");
-                            foreach (var y in motherb)
-                                Console.WriteLine(y);
+                            using (StreamWriter OutputFile = new StreamWriter("proba.txt", true))
+                            {
+                                OutputFile.Write($"{motherb[0]},{motherb[1]},{motherb[2]},{motherb[3]},{motherb[4]},\n");
+                            }
                             break;
                         case "graphics card":                                                           //gpu
                             gpu[1] = StringInput("Add meg a gyártóját: ");
-                            gpu[2] = StringInput("Integrált vagy dedikált?");
+                            gpu[2] = StringInput("Integrált vagy dedikált?: ");
                             gpu[3] = StringInput("Add meg a típusát: ");
                             gpu[4] = IntInput("Add meg az árát: ");
-                            foreach (var item in gpu)
+                            using (StreamWriter OutputFile = new StreamWriter("proba.txt", true))
                             {
-                                Console.WriteLine(item);
+                                OutputFile.Write($"{gpu[0]},{gpu[1]},{gpu[2]},{gpu[3]},{gpu[4]}\n");
                             }
                             break;
                         case "ram":                                                                     //ram
                             ram[1] = StringInput("Add meg a gyártóját: ");
                             ram[2] = IntInput("Add meg a méretét: ");
-                            ram[3] = StringInput("Add meg az órajelét");
+                            ram[3] = StringInput("Add meg az órajelét: ");
                             ram[4] = IntInput("Add meg az árát: ");
-                            foreach(var item in ram) Console.WriteLine(item);
+                            using (StreamWriter OutputFile = new StreamWriter("proba.txt", true))
+                            {
+                                OutputFile.Write($"{ram[0]},{ram[1]},{ram[2]},{ram[3]},{ram[4]}\n");
+                            }
                             break;
-                        case "storage":
+                        case "storage":                                                                 //storage
                             storage[1] = StringInput("Add meg a gyártóját: ");
                             storage[2] = StringInput("SSD vagy HDD: ");
                             storage[3] = IntInput("Add meg a kapacitását(GB): ");
                             storage[4] = IntInput("Add meg az árát: ");
+                            using (StreamWriter OutputFile = new StreamWriter("proba.txt", true))
+                            {
+                                OutputFile.Write($"{storage[0]},{storage[1]},{storage[2]},{storage[3]},{storage[4]}\n");
+                            }
                             break;
-                        case "monitor":
+                        case "monitor":                                                                 //monitor
                             monitor[1] = StringInput("Add meg a gyártóját:");
                             monitor[2] = IntInput("Add meg a méretét(inch): ");
                             monitor[3] = StringInput("Add meg a felbontását: ");
                             monitor[4] = IntInput("Add meg a képfrissítési gyakoriságot(hz): ");
                             monitor[5] = IntInput("Add meg az árát: ");
-                            foreach (var item in monitor)
+                            using (StreamWriter OutputFile = new StreamWriter("proba.txt", true))
                             {
-                                Console.WriteLine(item);
+                                OutputFile.Write($"{monitor[0]},{monitor[1]},{monitor[2]},{monitor[3]},{monitor[4]}\n");
                             }
                             break;
-                        case "keyboard":
+                        case "keyboard":                                                               //keyboard
                             keyb[1] = StringInput("Add meg a gyártóját: ");
                             keyb[2] = StringInput("Mechanikus vagy membrán: ");
                             keyb[3] = IntInput("Add meg az árát: ");
+                            using (StreamWriter OutputFile = new StreamWriter("proba.txt", true))
+                            {
+                                OutputFile.Write($"{keyb[0]},{keyb[1]},{keyb[2]},{keyb[3]}\n");
+                            }
                             break;
-                        case "mouse":
+                        case "mouse":                                                                   //mouse
                             mouse[1] = StringInput("Add meg a gyártóját: ");
                             mouse[2] = StringInput("Lézeres vagy optikai: ");
                             mouse[3] = IntInput("Add meg az árát: ");
+                            using (StreamWriter OutputFile = new StreamWriter("proba.txt", true))
+                            {
+                                OutputFile.Write($"{mouse[0]},{mouse[1]},{mouse[2]},{mouse[3]}\n");
+                            }
                             break;
                     }
                 }
                 else Console.WriteLine("Nincs ilyen lehetőség."); break;
             }
         }
-
         public void Readin() 
         {
             string upload;
