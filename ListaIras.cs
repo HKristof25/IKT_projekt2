88e6f563cfc1ca68b102624a904948670c2d51a6
DavidC112 <144622133+DavidC112@users.noreply.github.com>
2024-01-03 20:00:20 +0100

    Listába írás kész

---
+++ ListaIras.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alkatreszek
+{
+    internal class ListaIras
+    {
+        object elso;
+        object masodik;
+        object harmadik;
+        object negyedik;
+        object otodik;
+
+        public ListaIras(object elso, object masodik, object harmadik, object negyedik, object otodik )
+        {
+            this.elso = elso;
+            this.masodik = masodik;
+            this.harmadik = harmadik;
+            this.negyedik = negyedik; 
+            this.otodik = otodik;
+        }
+        public object Elso { get => elso; }
+        public object  Masodik { get => masodik; }
+        public object Harmadik { get => harmadik; }
+        public object Negyedik { get => negyedik; set => negyedik = value; }
+        public object Otodik{ get => otodik; set => otodik = value; }
+
+    }
+}
